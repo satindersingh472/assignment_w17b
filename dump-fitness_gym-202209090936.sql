003db006a8e10b62e9a509cd97105a731f2bf94b
satinder <satindersingh472@gmail.com>
2022-09-09 09:37:02 -0600

    added fitness employee

--- dump-fitness_gym-202209090936.sql
+++ dump-fitness_gym-202209090936.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `fitness_employee`
+--
+
+DROP TABLE IF EXISTS `fitness_employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fitness_employee` (
+  `first_name` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
+  `last_name` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
+  `position` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fitness_employee`
+--
+
+LOCK TABLES `fitness_employee` WRITE;
+/*!40000 ALTER TABLE `fitness_employee` DISABLE KEYS */;
+INSERT INTO `fitness_employee` VALUES ('sat','singh','ceo','2022-05-06',10000),('nav','grewal','manager','2022-06-07',5000),('sam','grewal','supervisor','2022-06-05',2500),('nit','kahlo','team member','2022-06-08',2500),('amy','dhaliwal','team member','2022-05-10',NULL);
+/*!40000 ALTER TABLE `fitness_employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping routines for database 'fitness_gym'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -28,4 +54,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-09  9:07:00
+-- Dump completed on 2022-09-09  9:36:03
